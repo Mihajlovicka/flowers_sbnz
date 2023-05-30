9ef5bacb0a74755f5283f547acf96b6f9decc4f1
Sara <saramihajlovic94@gmail.com>
2023-05-30 02:22:45 +0200

    backward

--- backend/service/src/test/java/com/ftn/sbnz/service/tests/RulesTest.java
+++ backend/service/src/test/java/com/ftn/sbnz/service/tests/RulesTest.java
@@ -46,26 +46,26 @@ public class RulesTest {
         ksession.insert(new SpacingBad(SpaceNeed.DENSE, SpaceNeed.DENSE));
         ksession.insert(new SpacingBad(SpaceNeed.AGGRESSIVE, SpaceNeed.AGGRESSIVE));
 
-        Size s = new Size(2,10,2,10);
-        Environment e1 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)),40,false,0.0,0.0, SpaceNeed.VERTICAL,new ArrayList(Arrays.asList(NutrientType.CALCIUM)));
-        Maintenance m1 = new Maintenance(1,"",WateringNeeds.AVERAGE,"","","", MaintenanceNeeds.LOW, PlantResistanceLevel.MODERATELY_RESISTANT);
-        Flower f = new Flower(true,new ArrayList<>(Arrays.asList(Season.SPRING)));
-        Description d1 = new Description(new ArrayList(Arrays.asList("red","blue")),s,"", PlantType.ANNUAL,f,new ArrayList<>(Arrays.asList(Season.SPRING)));
-        Plant p1 = new Plant("Orchid1",5., CareLevel.EASY,e1,d1,m1);
+        Size s = new Size(2, 10, 2, 10);
+        Environment e1 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)), 40, false, 0.0, 0.0, SpaceNeed.VERTICAL, new ArrayList(Arrays.asList(NutrientType.CALCIUM)));
+        Maintenance m1 = new Maintenance(1, "", WateringNeeds.AVERAGE, "", "", "", MaintenanceNeeds.LOW, PlantResistanceLevel.MODERATELY_RESISTANT);
+        Flower f = new Flower(true, new ArrayList<>(Arrays.asList(Season.SPRING)));
+        Description d1 = new Description(new ArrayList(Arrays.asList("red", "blue")), s, "", PlantType.ANNUAL, f, new ArrayList<>(Arrays.asList(Season.SPRING)));
+        Plant p1 = new Plant("Orchid1", 5., CareLevel.EASY, e1, d1, m1);
 
-        Environment e2 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)),60,true,0.0,0.0, SpaceNeed.COMPACT,new ArrayList(Arrays.asList(NutrientType.BORON)));
-        Maintenance m2 = new Maintenance(1,"",WateringNeeds.AVERAGE,"","","",MaintenanceNeeds.LOW,PlantResistanceLevel.HIGH_RESISTANT);
-        Description d2 = new Description(new ArrayList(Arrays.asList("red","violet")),s,"",PlantType.PERENNIAL,f,new ArrayList<>(Arrays.asList(Season.SPRING)));
-        Plant p2 = new Plant("Orchid2",5.,CareLevel.EASY,e2,d2,m2);
+        Environment e2 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)), 60, true, 0.0, 0.0, SpaceNeed.COMPACT, new ArrayList(Arrays.asList(NutrientType.BORON)));
+        Maintenance m2 = new Maintenance(1, "", WateringNeeds.AVERAGE, "", "", "", MaintenanceNeeds.LOW, PlantResistanceLevel.HIGH_RESISTANT);
+        Description d2 = new Description(new ArrayList(Arrays.asList("red", "violet")), s, "", PlantType.PERENNIAL, f, new ArrayList<>(Arrays.asList(Season.SPRING)));
+        Plant p2 = new Plant("Orchid2", 5., CareLevel.EASY, e2, d2, m2);
 
-        Environment e3 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)),40,false,0.0,0.0, SpaceNeed.COMPACT,new ArrayList(Arrays.asList(NutrientType.CALCIUM)));
-        Maintenance m3 = new Maintenance(1,"",WateringNeeds.HIGH,"","","",MaintenanceNeeds.LOW,PlantResistanceLevel.SENSITIVE);
-        Description d3 = new Description(new ArrayList(Arrays.asList("red","blue")),s,"",PlantType.ANNUAL,f,new ArrayList<>(Arrays.asList(Season.SPRING)));
-        Plant p3 = new Plant("Orchid3",5.,CareLevel.EASY,e2,d2,m2);
+        Environment e3 = new Environment(new ArrayList(Arrays.asList(Sunlight.PartialSun)), 40, false, 0.0, 0.0, SpaceNeed.COMPACT, new ArrayList(Arrays.asList(NutrientType.CALCIUM)));
+        Maintenance m3 = new Maintenance(1, "", WateringNeeds.HIGH, "", "", "", MaintenanceNeeds.LOW, PlantResistanceLevel.SENSITIVE);
+        Description d3 = new Description(new ArrayList(Arrays.asList("red", "blue")), s, "", PlantType.ANNUAL, f, new ArrayList<>(Arrays.asList(Season.SPRING)));
+        Plant p3 = new Plant("Orchid3", 5., CareLevel.EASY, e2, d2, m2);
 //
         PlantCareUserForm plantCareUserForm = new PlantCareUserForm(UserLevel.BEGINNER, PlantCareUserForm.Frequency.DAILY, PlantCareUserForm.ConsistencyLevel.INCONSISTENT);
         EnvironmentPreferences environmentPreferences = new EnvironmentPreferences(new ArrayList(Arrays.asList(Sunlight.PartialSun)), EnvironmentPreferences.Position.ByDoor, EnvironmentPreferences.Room.Kitchen);
-        LookPreferences pref = new LookPreferences(new ArrayList(Arrays.asList("red")), new Size(5,9,1,10),new ArrayList<>(Arrays.asList(Season.SPRING)),PlantType.ANNUAL,f);
+        LookPreferences pref = new LookPreferences(new ArrayList(Arrays.asList("red")), new Size(5, 9, 1, 10), new ArrayList<>(Arrays.asList(Season.SPRING)), PlantType.ANNUAL, f);
 
         HashMap<String, PlantRec> recommendedPlants = new HashMap<>();
         ArrayList<UserPlant> userPlants = new ArrayList<>();
@@ -80,64 +80,63 @@ public class RulesTest {
         ksession.insert(pref);
         ksession.setGlobal("recommendedPlants", recommendedPlants);
         ksession.insert(recommendedPlants);
-        userPlants.forEach( el -> ksession.insert(el));
+        userPlants.forEach(el -> ksession.insert(el));
         long ruleFireCount = ksession.fireAllRules();
         System.out.println(ruleFireCount);
-        for(String k: recommendedPlants.keySet()){
+        for (String k : recommendedPlants.keySet()) {
             System.out.println("k: " + k + " v: " + recommendedPlants.get(k).getPoints());
         }
     }
 
     @Test
-    public  void test1(){
+    public void test1() {
         KieServices ks = KieServices.Factory.get();
         KieContainer kc = ks.newKieClasspathContainer();
         KieSession ksession = kc.newKieSession();
 //        ksession.addEventListener(new DebugAgendaEventListener());
 
-        ksession.insert(new PlantName("Ruza"));
 
-        ksession.insert(new Disease("Prahasta plijesan", "Ruza"));
-        ksession.insert(new Symptom("zucenje lisca","Prahasta plijesan"));
-        ksession.insert(new Cause("Visoka vlaznost", "zucenje lisca"));
-        ksession.insert(new Cause("Losa cirkulacija vazduha", "zucenje lisca"));
-        ksession.insert(new Treatment("Uklonite zarazeno lisce", "zucenje lisca"));
+        // Inserted data
+        ksession.insert(new Disease("Truljenje korena", "Orhideja"));
+        ksession.insert(new Symptom("Promena boje korena", "Truljenje korena"));
+        ksession.insert(new Symptom("Truljenje korena", "Truljenje korena"));
+        ksession.insert(new Symptom("Opadanje lisca", "Truljenje korena"));
+        ksession.insert(new Cause("Prekomerno zalivanje", "Promena boje korena"));
+        ksession.insert(new Cause("Losa drenaza supstrata", "Promena boje korena"));
+        ksession.insert(new Cause("Prekomerno zalivanje", "Truljenje korena"));
+        ksession.insert(new Cause("Losa drenaza supstrata", "Truljenje korena"));
+        ksession.insert(new Cause("Kontaminirani supstrat", "Truljenje korena"));
+        ksession.insert(new Treatment("Uklanjanje trulih korena", "Promena boje korena"));
+        ksession.insert(new Treatment("Presadjivanje u svez supstrat", "Promena boje korena"));
+        ksession.insert(new Treatment("Uklanjanje trulih korena", "Truljenje korena"));
+        ksession.insert(new Treatment("Presadjivanje u svez supstrat", "Truljenje korena"));
+        ksession.insert(new Treatment("Smanjenje zalivanja", "Truljenje korena"));
+        ksession.insert(new Treatment("Primena fungicida", "Truljenje korena"));
+        ksession.insert(new Treatment("Poboljsanje cirkulacije vazduha", "Opadanje lisca"));
+        ksession.insert(new Treatment("Pravilan rezim zalivanja", "Opadanje lisca"));
+        ksession.insert(new Treatment("Hranjenje biljke", "Opadanje lisca"));
 
-        ksession.insert(new Disease("Prahasta plijesan", "Ruza"));
-        ksession.insert(new Symptom("Uvenuce", "Prahasta plijesan"));
-        ksession.insert(new Cause("Visoka vlaznost", "Uvenuce"));
-        ksession.insert(new Treatment("Uklonite zarazeno lisce", "Uvenuce"));
-//
-        ksession.insert(new Symptom("Braon mrlje", "Mrlje na listovima"));
-        ksession.insert(new Disease("Mrlje na listovima", "Ruza"));
-        ksession.insert(new Cause("Gljivicna infekcija", "Braon mrlje"));
-        ksession.insert(new Treatment("Nanesite fungicid", "Braon mrlje"));
-//
-        ksession.insert(new PlantName("Ljiljan"));
-        ksession.insert(new Symptom("zucenje lisca", "Nedostatak hranljivih materija"));
-        ksession.insert(new Disease("Nedostatak hranljivih materija", "Ljiljan"));
-        ksession.insert(new Cause("Nedostatak azota", "zucenje lisca"));
-        ksession.insert(new Cause("Desebalans pH vrednosti zemljista", "zucenje lisca"));
-        ksession.insert(new Treatment("Djubrite zemlju", "zucenje lisca"));
-//
+        ksession.insert(new Disease("Virus mozaika", "Orhideja"));
+        ksession.insert(new Symptom("Promene na liscu", "Virus mozaika"));
+        ksession.insert(new Symptom("Promene u boji cveta", "Virus mozaika"));
+        ksession.insert(new Cause("Infekcija virusom", "Promene na liscu"));
+        ksession.insert(new Cause("Infekcija virusom", "Promene u boji cveta"));
+        ksession.insert(new Treatment("Uklanjanje zarazenih delova biljke", "Promene na liscu"));
+        ksession.insert(new Treatment("Uklanjanje zarazenih cvetova", "Promene u boji cveta"));
+
+
+
         ksession.insert(new PlantName("Orhideja"));
-        ksession.insert(new Symptom("Uvenuce", "Preveliko zalivanje"));
-        ksession.insert(new Disease("Preveliko zalivanje", "Orhideja"));
-        ksession.insert(new Cause("Prekomerno zalivanje", "Uvenuce"));
-        ksession.insert(new Cause("Losa drenaza", "Uvenuce"));
-        ksession.insert(new Treatment("Prilagodite raspored zalivanja", "Uvenuce"));
+        ksession.insert("Promena boje korena");
+        ksession.insert("Truljenje korena");
+        ksession.insert("Opadanje lisca");
+        ksession.insert("Promene na liscu");
+
+        
         long ruleFireCount = 0;
-//        ksession.insert( "symptoms of" );
-//        ruleFireCount = ksession.fireAllRules();
-//        System.out.println("---" + ruleFireCount);
-
-        ksession.insert( "plantAndCauses" );
         ruleFireCount = ksession.fireAllRules();
         System.out.println("---" + ruleFireCount);
 
-        ksession.insert( "plant disease treatman" );
-        ruleFireCount = ksession.fireAllRules();
-        System.out.println("---" + ruleFireCount);
 
     }
 
